eb0f744dbf7b29d7660df3a344953f2e9716bd0f
trxsoftonics <trxsoftonics@gmail.com>
2025-03-28 18:21:16 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate =  new Date("March 25, 2025 15:37:25").getTime();
+        var finalDate =  new Date("April 30, 2025 15:37:25").getTime();
         //-date: "Mar 25 2021",
 
         $('.home-content__clock').countdown(finalDate)
